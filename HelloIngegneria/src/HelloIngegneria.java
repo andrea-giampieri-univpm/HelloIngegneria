717273473023c05759d79dbd194933f7c2bdfc20
ClaudioSirocchi <s1082025@studenti.univpm.it>
2019-03-17 12:43:09 +0100

    Modifica codice

--- HelloIngegneria/src/HelloIngegneria.java
+++ HelloIngegneria/src/HelloIngegneria.java
@@ -2,7 +2,7 @@
 public class HelloIngegneria {
 
 	public static void main(String[] args) {
-		String nome="Claudio";
+		String nome="Claudio"; //inserisci il tuo nome fra le virgolette 
 		System.out.println("Hello Ingegneria I'm "+nome+"!");
 	}
 
